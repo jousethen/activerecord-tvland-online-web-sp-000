4375dd1c07d04468602e03fd7ddded0ddf9e4794
Jouse then <jthen93@gmail.com>
2021-04-03 00:58:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,7 +2,7 @@ class Show < ActiveRecord::Base
   has_many :characters
   has_many :actors, through: :characters
   belongs_to :network
-  has_one :genre
+  
   def actors_list
     self.actors.collect do |actor|
       actor.full_name
